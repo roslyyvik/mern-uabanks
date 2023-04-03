0d9d7e3518fffc74d6c19a67a66724323ae01380
Viktor Rosvik <roslyyvik@gmail.com>
2023-04-03 18:43:31 +0300

    server add allow-methods

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,10 @@ app.use( helmet({
 // app.use(cors())
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://res.cloudinary.com/");
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
